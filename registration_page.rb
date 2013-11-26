d7e132e3044e495b0a6388495e58f9da6ad18c1c
sage0007 <srimal@rent.com>
2013-11-26 14:29:41 -0800

    some codes

---
+++ registration_page.rb
@@ -0,0 +1,19 @@
+class RegistrationPage
+  include PageObject
+  # To change this template use File | Settings | File Templates.
+  sign_in_button=""
+  log_in_button=""
+  site_url=""
+
+  #method
+
+  def sign_in
+
+  end
+
+
+  def log_in
+
+  end
+
+end
---
+++ search_page.rb
@@ -0,0 +1,3 @@
+class SearchPage
+  # To change this template use File | Settings | File Templates.
+end
