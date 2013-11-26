a1643c408e91056443a8fee04b0060ef2f140cd9
sage0007 <srimal@rent.com>
2013-11-26 14:58:46 -0800

    Properties file

--- registration_page.rb
+++ registration_page.rb
@@ -1,15 +1,32 @@
-class RegistrationPage
-  include PageObject
+require 'yaml'
+require 'selenium-webdriver'
+require 'test/unit'
+
+
+class RegistrationPage < Test::Unit::TestCase
+ #include PageObject
   # To change this template use File | Settings | File Templates.
+
+  #Instantiate browser and read from yaml file
+
   sign_in_button=""
   log_in_button=""
   site_url=""
 
-  #method
-
-  def sign_in
-
+  def setUp
+    @driver= Selenium::WebDriver.for :firefox
+    @properties = YAML.load('properties.yaml')
+    @url=@properties['url']['qa']
   end
+
+  def teardown
+    @driver.close
+  end
+
+
+  def test_sign_in
+   puts @url
+   end
 
 
   def log_in
